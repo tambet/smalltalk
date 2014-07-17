92aaebdaa238e2c85842f05a4c55fa080b7989cb
Tambet Masik <tambet.masik@gmail.com>
2014-07-17 22:53:39 +0300

    API routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
   get 'home/index'
   root 'home#index'
+
+  namespace :api, defaults: {format: :json} do
+    namespace :v1 do
+      post 'signup'   => 'users#create'
+      post 'login'    => 'sessions#create'
+      delete 'logout' => 'sessions#destroy'
+
+      get 'login'     => 'sessions#create'
+      get 'logout'    => 'sessions#destroy'
+
+      resources :posts
+    end
+  end
+
 end
