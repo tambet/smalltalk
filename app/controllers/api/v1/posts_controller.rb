7bb34ea298f90d839f1a284ff5f2fbd2c6240c06
Tambet Masik <tambet.masik@gmail.com>
2014-07-17 23:03:16 +0300

    Implement GET /api/v1/posts

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,6 +1,12 @@
 module Api
   module V1
     class PostsController < ApplicationController
+      respond_to :json
+
+      def index
+        respond_with Post.all
+      end
+
     end
   end
 end
