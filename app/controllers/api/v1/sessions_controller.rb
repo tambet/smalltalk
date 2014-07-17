89951e9daca0f41c289d8d56ae8caf53182f5af8
Tambet Masik <tambet.masik@gmail.com>
2014-07-17 23:06:35 +0300

    Implement session create and destroy

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,6 +2,22 @@ module Api
   module V1
     class SessionsController < ApplicationController
       skip_before_action :authenticate
+
+      def create
+        user = User.first #.authenticate(params[:email], params[:password])
+        if user
+          session[:user_id] = user.id
+          render status: 200, json: 'OK'
+        else
+          render status: 401, json: 'Unauthorized'
+        end
+      end
+
+      def destroy
+        session[:user_id] = nil
+        render status: 200, json: 'OK'
+      end
+
     end
   end
 end
