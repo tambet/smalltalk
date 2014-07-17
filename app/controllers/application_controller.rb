777e696b00099b87fe496b6e8a86b71bbb19fb4a
Tambet Masik <tambet.masik@gmail.com>
2014-07-17 22:58:09 +0300

    authenticate before_action added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,18 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  before_action :authenticate
+  protect_from_forgery with: :null_session
+
+  protected
+  def authenticate
+    authenticate_cookie || render_unauthorized
+  end
+
+  def authenticate_cookie
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def render_unauthorized
+    render json: 'Unauthorized', status: 401
+  end
+  
 end
