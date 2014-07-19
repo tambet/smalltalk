ef6085541b325f36bde6f80b32e173af0799bdb3
Tambet Masik <tambet.masik@gmail.com>
2014-07-20 01:12:18 +0300

    Post create added

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -7,6 +7,21 @@ module Api
         respond_with Post.all
       end
 
+      def create
+        post = Post.new(post_params)
+        post.user = @current_user
+        if post.save
+          render json: post
+        else
+          render status: 400, json: post.errors.full_messages
+        end
+      end
+
+      private
+      def post_params
+        params.permit(:heading, :body)
+      end
+
     end
   end
 end
--- spec/controllers/api/v1/posts_controller_spec.rb
+++ spec/controllers/api/v1/posts_controller_spec.rb
@@ -6,13 +6,31 @@ RSpec.describe Api::V1::PostsController, :type => :controller do
 
   it "with no authentication" do
     get :index, format: :json
-    expect(response.status).to eq(401)
+    expect(response.status).to eq 401
   end
 
   it "with authentication" do
     authWithUser(user)
     get :index, format: :json
     expect(response).to be_success
+  end
+
+  describe "POST create" do
+    before(:each) { authWithUser(user) }
+
+    describe "with valid params" do
+      it "creates a new Post" do
+        post :create, {heading: 'Heading', body: 'Body'}
+        expect(response).to be_success
+      end
+    end
+
+    describe "with invalid params" do
+      it "returns all errors" do
+        post :create, {heading: 'Heading'}
+        expect(response.body).to eq "[\"Body can't be blank\"]"
+      end
+    end
   end
 
 end
